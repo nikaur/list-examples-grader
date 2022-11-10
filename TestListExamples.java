0953389db2c42ce47bc865b577db139aae0f49ee
nimkaur <67022580+nimkaur@users.noreply.github.com>
2022-11-10 10:39:21 -0800

    autograder

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,59 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class TestListExamples {
-  // Write your grading tests here!
+    //generic test 
+    @Test
+      public void mergeTest1(){
+        List<String> s1 = Arrays.asList("apple", "boy"); 
+        List<String> s2 = Arrays.asList("are", "zed");
+        List<String> s3 = Arrays.asList("apple", "are", "boy", "zed");  
+        assertEquals(s1, ListExamples.merge(s1, s2));
+      }
+
+    @Test 
+      public void mergeTest2(){
+        List<String> s1 = Arrays.asList("a", "list", "long", "very"); 
+        List<String> s2 = Arrays.asList("another", "list", "long"); 
+        List<String> s3 = Arrays.asList("a", "another", "list", "list", "long", "long", "very"); 
+        assertEquals(s3, ListExamples.merge(s1, s2));
+      }
+
+      @Test
+      public void mergeTest3(){
+        List<String> s1 = new ArrayList<>();  
+        List<String> s2 = new ArrayList<>();
+        List<String> s3= new ArrayList<>(); 
+        assertEquals(s3, ListExamples.merge(s1, s2));
+      }
+
+      @Test
+      public void filterTest1(){
+          List<String> input = Arrays.asList("hi", "hello", "k", "hm"); 
+          //String a = "h";
+          StringChecker sc = new StringChecker(){
+              public boolean checkString(String s){
+                  return true; 
+              } 
+          };  
+          assertEquals(input, ListExamples.filter(input,sc));
+        }
+
+      @Test
+      public void filterTest2(){
+        List<String> s1 = Arrays.asList("apple", "berry", "smth"); 
+        List<String> s2 = new ArrayList<>(); 
+        StringChecker sc = new StringChecker(){
+            public boolean checkString(String s){
+              return false; 
+            }
+        }; 
+        assertNotEquals(s1, ListExamples.filter(s1, sc));
+        assertEquals(s2, ListExamples.filter(s1, sc));
+      }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,75 @@
 # Create your grading script here
 
-set -e
-
 rm -rf student-submission
 git clone $1 student-submission
 
+#useful variables 
+CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
+#copy over our written tests to the student-submission directory 
+cp TestListExamples.java student-submission
+cp -r lib student-submission
+#go into the student-submission directory 
+cd student-submission 
+
+
+#check if ListExamples.java is a file 
+if [[ -f ListExamples.java ]]
+then 
+    echo "File detected."
+else 
+    echo "Error: File not detected. Please submit the correct files"
+    exit 1 
+fi 
+
+
+#check if javac is working 
+javac -cp $CP *.java 
+
+if [[ -f ListExamples.class ]]
+then #it's working 
+    echo "javac Compiled"
+else #javac is not working
+    echo "Compile error"
+    exit 1 
+fi 
+
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > testanswers.txt 
+grep "mergeTest1*" testanswers.txt 
+
+# grep for all the tests --- then find how many failed to find the score 
+# start from full score + deduct by 1 for every failed test 
+
+echo 5
+let a=5 
+if [[ $Comp == "mergeTest1" ]]
+then
+    let a-1
+    echo a
+fi 
+
+if [[ grep "mergeTest2" testanswers.txt ]]
+then
+    let a-1
+    echo a
+fi 
+
+if [[ grep "mergeTest3" testanswers.txt ]]
+then
+    let a-1
+    echo a
+fi 
+
+if [[ grep "filterTest1" testanswers.txt ]]
+then
+    let a-1
+    echo a
+fi 
+
+if [[ grep "filterTest2" testanswers.txt ]]
+then
+    let a-1
+    echo a
+fi
+
+echo "Score is: " $a " out of 5"
